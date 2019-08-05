3abf6c02c74857ce48370adcc711f22d174ac492
I310538 <eamonn.o.broithe@sap.com>
2019-08-05 17:22:09 +0100

    Enhance redux state implementation

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,40 @@
-import { creatStore, createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { defaultState } from '../../server/defaultState'
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+import * as sagas from './sagas.mock'
+import * as mutations from './mutations'
 
 export const store = createStore(
-	function reducer(state = defaultState, action) {
-		return state;
-	}
+	combineReducers({
+		tasks(tasks = defaultState.tasks, action) {
+			switch (action.type) {
+				case mutations.CREATE_TASK:
+					return [...tasks, {
+						id: action.taskId,
+						name: "New Task",
+						group: action.groupId,
+						owner: action.ownerId,
+						isComplete: false
+					}]
+			}
+			return tasks;
+		},
+		comments(comments = defaultState.comments) {
+			return comments;
+		},
+		users(users = defaultState.users) {
+			return users;
+		},
+		groups(groups = defaultState.groups) {
+			return groups;
+		},
+	}),
+	applyMiddleware(createLogger(), sagaMiddleware)
 )
+
+for (let saga in sagas) {
+	sagaMiddleware.run(sagas[saga])
+}
---
+++ src/app/store/sagas.mock.js
@@ -0,0 +1,18 @@
+import {
+	take,
+	put,
+	select
+} from 'redux-saga/effects'
+
+import * as mutations from './mutations'
+import uuid from 'uuid'
+
+export function* taskCreationSaga() {
+	while (true) {
+		const { groupId } = yield take(mutations.REQUEST_TASK_CREATION);
+		console.log("Got groupId", groupId)
+		const ownerId = 'U1';
+		const taskId = uuid();
+		yield put(mutations.createTask(taskId, groupId, ownerId))
+	}
+}
